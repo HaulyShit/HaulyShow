95f068f8f7116002edaa7819e5848f170b07a92e
Janis Hau <j.hau@neusta.de>
2016-04-26 14:52:46 +0200

    HaulyShow, rudimentary setup

---
+++ assets/js/haulyshow.js
@@ -0,0 +1,161 @@
+/*********************************************************************************
+
+AUTOR INFORMATION
+********************
+
+@AUTHOR:            Janis Hau
+@AUTHOR_EMAIL:      hau@haulyshit.de
+@AUTHOR_WEBSITE:    haulyshit.de
+****************************************
+
+
+INTRODUCE
+**********
+
+- This script file ist jQuery (v.2.xxx) based.
+- All function-snippets will be write in their own namend function and they will
+  be called in the resize function. So far it is neccessary. ;)
+
+
+RESIZE
+**********
+
+- The resize() will be used for the methodlogy "don't repeat yourself". So,
+  you call it in the document ready scope, resize and something else.
+- In the head of the resize() call are all variables they are needed.
+- All objects should be give as a string.
+
+
+DECLARED VARS
+********************
+
+- $var this vars are dom-cached objects.
+
+
+MENU
+**********
+
+The menu() function...
+- ...activate the canvas mobile menu 
+- ...and prepend the trigger.
+
+
+Variable function names
+******************************
+
+- "nc" or "navcon"    the container there contain the navtrigger, navigation,...
+- "nav"               the container the contains the nav
+- nt                  the navtrigger or burger-menu or something like that 
+
+*********************************************************************************/
+function menu(navCon, nav, navTrigger) {
+    var $nc = $(navCon),
+        $n = $(nav),
+        $nt = $(navTrigger),
+        nt = navTrigger.replace(/#|./, ''),
+        nts = '<div class="' + nt + '"><hr/><hr/><hr/></div>';
+
+    // APPEND navTriggerStructur
+    if ($nt.length == 0) {
+        $nc.prepend(nts);
+    }
+
+    //EVENT ON CLICK
+    $(navTrigger).click(function () {
+        $(this).toggleClass('active');
+        $n.toggleClass('active');
+    });
+
+}
+
+
+
+
+
+/*********************************************************************************
+haulyShow
+*********
+
+haulyShow is an easy responsiv percent based slideShow.
+
+- At the default one image per screen.
+*********************************************************************************/
+var count = 0;
+
+function haulyShow() {
+    var $haulyShow = $('.haulyShow'),
+        $figure = $haulyShow.children('figure'),
+        npos = 0;
+
+    // - Append the viewport
+    // - Append the controls
+    if ($('.haulyShow__viewport').length == 0) {
+
+        $haulyShow.append('<div class="haulyShow__viewport" style="width: ' + (100 * $figure.length) + '%" data-pos="0%"></div><div class="haulyShow__controls"><button class="haulyshow--prev">PREV</button><button class="float--right haulyshow--next">NEXT</button></div>');
+    }
+
+    //- Give the slides, the width in percent
+    // - Append the slides in the viewport
+    // - Write the Data-images/caption in the figures
+    $figure.css('width', (100 / $figure.length) + '%').appendTo('.haulyShow__viewport').each(function () {
+        var imgUrl = $(this).attr('data-image'),
+            caption = $(this).attr('data-caption');
+
+        $(this).append('<img src="' + imgUrl + '" /><figcaption>' + caption + '</figcaption');
+    });
+
+
+    //Next and previous function
+    function haulyShowNext() {
+        if (count < $figure.length - 1) {
+            count++;
+            npos = 100 * (count * -1) + '%';
+            $haulyShow.children('.haulyShow__viewport').css('left', npos).attr('data-pos', npos);
+        }
+    }
+
+    function haulyShowPrev() {
+        if (count > 0 && count > $figure.length - 1) {
+            count--;
+            npos = 100 * (count * -1) + '%';
+            $haulyShow.children('.haulyShow__viewport').css('left', npos).attr('data-pos', npos);
+        }
+    }
+
+    //Triggers
+    // NEXT BUTTON
+    $('.haulyshow--next').click(function () {
+        haulyShowNext();
+    });
+    //PREV BUTTON
+    $('.haulyshow--prev').click(function () {
+        haulyShowPrev();
+    });
+
+}
+
+
+/*********************************************************************************
+#RESIZE
+*********************************************************************************/
+function resize() {
+    var windowWidth = $(window).width(),     // window width
+        windowHeight = $(window).height(),   // window height
+        mobileWidth = 640
+
+    haulyShow();
+}
+//RESIZE END
+
+
+// $(function() {
+$(document).ready(function () {
+    haulyShow();
+    resize();
+});
+
+$(window).resize(function () {
+    resize();
+});
+
+// });
