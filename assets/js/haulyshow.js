462062d75ee40482f231270728396b6a774594ef
Janis Hau <j.hau@neusta.de>
2016-04-27 14:43:42 +0200

    Little bit more customizable, little big fixing

--- assets/js/haulyshow.js
+++ assets/js/haulyshow.js
@@ -1,120 +1,107 @@
 /*********************************************************************************
-
-AUTOR INFORMATION
-********************
-@AUTHOR:            Janis Hau
-@AUTHOR_EMAIL:      hau@haulyshit.de
-@AUTHOR_WEBSITE:    haulyshit.de
-****************************************
-
-INTRODUCE
-**********
-- This script file ist jQuery (v.2.xxx) based.
-- All function-snippets will be write in their own namend function and they will
-  be called in the resize function. So far it is neccessary. ;)
-
-
-RESIZE
-**********
-- The resize() will be used for the methodlogy "don't repeat yourself". So,
-  you call it in the document ready scope, resize and something else.
-- In the head of the resize() call are all variables they are needed.
-- All objects should be give as a string.
-
-
-DECLARED VARS
-********************
-- $var this vars are dom-cached objects.
-
-*********************************************************************************/
-
-
-
-
-
-/*********************************************************************************
-haulyShow
-*********
-
-haulyShow is a rudimentary responsiv percentage based slideShow.
-- One figure = one slide
-*********************************************************************************/
+ * AUTOR INFORMATION
+ * ********************
+ * @AUTHOR:            Janis Hau
+ * @AUTHOR_EMAIL:      hau@haulyshit.de
+ * @AUTHOR_WEBSITE:    haulyshit.de
+ *
+ *
+ * INTRODUCE
+ * **********
+ * haulyShow is a rudimentary responsiv, percent-based slideShow.
+ *
+ * - One figure = one slide
+ * - This script file ist jQuery (v.2.xxx) based.
+ * - All function-snippets will be write in their own namend function and they
+ *   will be called in the resize function. So far it is neccessary. ;)
+ ********************************************************************************/
 var count = 0;
 
-function haulyShow() {
-    var $haulyShow = $('.haulyShow'),
-        $figure = $haulyShow.children('figure'),
-        npos = 0;
+function haulyShow ( ) {
+	// @formatter:off
+		var $haulyShow	= $ ( '.haulyShow' ) ,
+			viewport	= 'haulyShow-viewport',
+			controls	= 'haulyShow-controls',
+			prev		= 'haulyshow-prev',
+			next		= 'haulyshow-next',
+			slide		= 'haulyShow-slide',
+		    $figure		= $haulyShow.children ( '.'+slide ) ,
+		    npos		= 0;
+		// @formatter:on
 
-    // - Append the viewport
-    // - Append the controls
-    if ($('.haulyShow__viewport').length == 0) {
+	// - Append the viewport
+	// - Append the controls
+	if ( $ ( '.' + viewport ).length == 0 ) {
+		// @formatter:off
+			$haulyShow.append ( 
+				'<div class="'+viewport+'" style="width: ' + ( 100 * $figure.length ) + '%" data-pos="0%"></div>' +
+					'<div class="'+controls+'">' + '<button class="'+prev+'">PREV</button>' + 
+					'<button class="float--right '+next+'">NEXT</button>' + 
+				'</div>' );
+			// @formatter:on
+	}
 
-        $haulyShow.append('<div class="haulyShow__viewport" style="width: ' + (100 * $figure.length) + '%" data-pos="0%"></div><div class="haulyShow__controls"><button class="haulyshow--prev">PREV</button><button class="float--right haulyshow--next">NEXT</button></div>');
-    }
+	// - Give the slides, the width in percent
+	// - Append the slides in the viewport
+	// - Write the Data-images/caption in the figures
+	$figure.css ( 'width' , ( 100 / $figure.length ) + '%' ).appendTo ( '.' + viewport ).each ( function ( ) {
 
-    // - Give the slides, the width in percent
-    // - Append the slides in the viewport
-    // - Write the Data-images/caption in the figures
-    $figure.css('width', (100 / $figure.length) + '%').appendTo('.haulyShow__viewport').each(function () {
-        var imgUrl = $(this).attr('data-image'),
-            caption = $(this).attr('data-caption');
+		var imgUrl = $ ( this ).attr ( 'data-image' ) ,
+		    caption = $ ( this ).attr ( 'data-caption' );
 
-        $(this).append('<img src="' + imgUrl + '" /><figcaption>' + caption + '</figcaption');
-    });
+		if ( typeof imgUrl !== typeof undefined && imgUrl !== false ) {
+			$ ( this ).append ( '<img src="' + imgUrl + '" />' );
+		}
+		if ( typeof caption !== typeof undefined && caption !== false ) {
+			$ ( this ).append ( '<figcaption>' + caption + '</figcaption' );
+		}
+
+	} );
+
+	function haulyShowSlideActive ( ) {
+		$ ( '.' + slide ).removeClass ( 'active' );
+		$ ( '.' + slide + ':nth-child(' + ( count + 1 ) + ')' ).addClass ( 'active' );
+	}
 
 
-    //Next and previous function
-    function haulyShowNext() {
-        if (count < $figure.length - 1) {
-            count++;
-            npos = 100 * (count * -1) + '%';
-            $haulyShow.children('.haulyShow__viewport').css('left', npos).attr('data-pos', npos);
-        }
-    }
+	//Next and previous function
+	function haulyShowNext ( ) {
+		if ( count < $figure.length - 1 ) {
+			count++;
+			npos = 100 * ( count * -1 ) + '%';
+			$haulyShow.children ( '.' + viewport ).css ( 'left' , npos ).attr ( 'data-pos' , npos );
+			haulyShowSlideActive ( );
+		}
+	}
 
-    function haulyShowPrev() {
-        if (count > 0 && count > $figure.length - 1) {
-            count--;
-            npos = 100 * (count * -1) + '%';
-            $haulyShow.children('.haulyShow__viewport').css('left', npos).attr('data-pos', npos);
-        }
-    }
 
-    //Triggers
-    // NEXT BUTTON
-    $('.haulyshow--next').click(function () {
-        haulyShowNext();
-    });
-    //PREV BUTTON
-    $('.haulyshow--prev').click(function () {
-        haulyShowPrev();
-    });
+	// .haulyShow-slide:nth-child(2)
+	// .haulyShow-slide:nth-child(2)
+
+	function haulyShowPrev ( ) {
+		if ( count > 0 && count >= $figure.length - 1 ) {
+			count--;
+			npos = 100 * ( count * -1 ) + '%';
+			$haulyShow.children ( '.' + viewport ).css ( 'left' , npos ).attr ( 'data-pos' , npos );
+			haulyShowSlideActive ( );
+		}
+	}
+
+
+	//Triggers
+	// NEXT BUTTON
+	$ ( '.' + next ).click ( function ( ) {
+		haulyShowNext ( );
+	} );
+	//PREV BUTTON
+	$ ( '.' + prev ).click ( function ( ) {
+		haulyShowPrev ( );
+	} );
 
 }
 
 
-/*********************************************************************************
-#RESIZE
-*********************************************************************************/
-function resize() {
-    var windowWidth = $(window).width(),     // window width
-        windowHeight = $(window).height(),   // window height
-        mobileWidth = 640
 
-    haulyShow();
-}
-//RESIZE END
-
-
-// $(function() {
-$(document).ready(function () {
-    haulyShow();
-    resize();
-});
-
-$(window).resize(function () {
-    resize();
-});
-
-// });
+jQuery ( document ).ready ( function ( ) {
+	haulyShow ( );
+} );
